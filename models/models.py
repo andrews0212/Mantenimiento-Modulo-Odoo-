80e87c55aeb6e0f8c18a2031afe4a73c7e62b98e
andrews0212 <andrewsdosramos@gmail.com>
2025-02-24 15:09:26 +0100

    ✅ Registro de Equipos: Permite registrar cada equipo con detalles como nombre, categoría, ubicación, número de serie y proveedor.

--- models/models.py
+++ models/models.py
@@ -2,14 +2,14 @@
 from odoo import models, fields, api
 
 class EquipmentCategory(models.Model):
-    _name = 'equipo.categoria'  # Nombre correcto del modelo
+    _name = 'mantenimiento.categoria'  # Nombre correcto del modelo
     _description = 'Categoría de Equipos'
 
     name = fields.Char(string="Nombre categoría", required=True)
     description = fields.Text(string="Descripción")
 
 class Proveedor(models.Model):
-    _name = 'equipo.proveedor'  # Nombre más claro para el modelo
+    _name = 'mantenimiento.proveedor'  # Nombre más claro para el modelo
     _description = 'Proveedor de Equipos'
 
     name = fields.Char(string='Nombre', required=True)
@@ -17,14 +17,13 @@ class Proveedor(models.Model):
     telefono = fields.Char(string="Teléfono")
     email = fields.Char(string="Correo electrónico")
     direccion = fields.Text(string="Dirección")
-
 class Equipo(models.Model):
     _name = "mantenimiento.equipo"
     _description = "Equipos de mantenimiento"
 
     name = fields.Char(string="Nombre", required=True)
-    category_id = fields.Many2one("equipo.categoria", string="Categoría")
+    category_id = fields.Many2one("mantenimiento.categoria", string="Categoría")
     description = fields.Text(string="Descripción")
     ubicacion = fields.Char(string="Ubicación")
     numeroSerie = fields.Char(string="Número de Serie")
-    proveedor_id = fields.Many2one("equipo.proveedor", string="Proveedor")  # Cambié a proveedor_id para seguir la convención
+    proveedor_id = fields.Many2one("mantenimiento.proveedor", string="Proveedor")

4e429fd3abab0aa1ad31e93774d96008397d1c36
andrews0212 <andrewsdosramos@gmail.com>
2025-02-24 21:20:12 +0100

    grega vistas y modelo de orden de mantenimiento.
    Introduce el modelo `MantenimientoOrden` para la gestión de órdenes de mantenimiento. Agrega las vistas de formulario y lista correspondientes. Actualiza el control de acceso y los menús para incluir la nueva funcionalidad de orden de mantenimiento.
    
    Correcciones #123

--- models/models.py
+++ models/models.py
@@ -27,3 +27,22 @@ class Equipo(models.Model):
     ubicacion = fields.Char(string="Ubicación")
     numeroSerie = fields.Char(string="Número de Serie")
     proveedor_id = fields.Many2one("mantenimiento.proveedor", string="Proveedor")
+
+class MantenimientoOrden(models.Model):
+    _name = "mantenimiento.orden"
+    _description = "Orden de Mantenimiento"
+
+    tipo_mantenimiento = fields.Selection([
+        ('preventivo', 'Preventivo'),
+        ('correctivo', 'Correctivo')
+    ], string="Tipo de Mantenimiento", required=True)
+
+    fecha_programada = fields.Date(string="Fecha Programada")
+    fecha_realizacion = fields.Date(string="Fecha de Realización")
+    equipo_id = fields.Many2one('mantenimiento.equipo', string="Equipo", required=True)
+    estado = fields.Selection([
+        ('pendiente', 'Pendiente'),
+        ('en_progreso', 'En Progreso'),
+        ('finalizado', 'Finalizado')
+    ], string="Estado", default='pendiente')
+    descripcion = fields.Text(string="Descripción")
